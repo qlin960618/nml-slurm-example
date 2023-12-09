93a20ca60ce74c8bdb2c494b429a15a968f23c03
qlin960618 <qlin1806@gmail.com>
2023-12-09 15:29:50 +0900

    need better ip discovery method

--- Training/jobs/torchrun.sh
+++ Training/jobs/torchrun.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-nodes=( $( scontrol show hostnames $SLURM_JOB_NODELIST ) )
-nodes_array=($nodes)
-head_node=${nodes_array[0]}
-head_node_ips=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --all-ip-addresses)
-head_node_ips_array=($head_node_ips)
-head_node_ip=${head_node_ips_array[0]}
 
 source /opt/anaconda3/etc/profile.d/conda.sh
 ########################
@@ -16,8 +10,8 @@ conda activate [YOUR_CONDA_ENVIRONMENT]
 
 torchrun --nnodes=$SLURM_NNODES \
     --node_rank=$SLURM_NODEID \
-    --nproc_per_node=$SLURM_GPUS_ON_NODE\
-    --master_addr=$head_node_ip --master_port=8344 \
+    --nproc_per_node=$SLURM_GPUS_PER_NODE\
+    --master_addr=$1 --master_port=8344 \
     Training/run_torchrun_train.py \
     --epochs=100 \
     --batch_size=12 \
--- Training/jobs/train_job.sh
+++ Training/jobs/train_job.sh
@@ -5,10 +5,20 @@
 ## NOTE: nnodes is the number of nodes you want to use
 ## NOTE: ntasks-per-node should be one, and let the child script handle the number of GPUs
 ## NOTE: gpus is the total number of GPUs you want to use
+## NOTE: Everything beside --gpus seem to be very specific, modifying them may cause command in the torchrun.sh script
+#         to hang at srun grabbing head_node ip. alternatively, can
 #######################################################################################################
 #SBATCH --exclusive --nodes=2 --ntasks-per-node=1 --gpus=7
 
 #SBATCH --chdir=/home/[USERNAME]/[USERNAME]_data/distributed_dataparallel_training-example
 #SBATCH --output=/home/[USERNAME]/[USERNAME]_data/distributed_dataparallel_training-example/out/%x-%j.out
 
-srun -l --ntasks-per-node=1  Training/jobs/torchrun.sh
+
+nodes=( $( scontrol show hostnames $SLURM_JOB_NODELIST ) )
+nodes_array=($nodes)
+head_node=${nodes_array[0]}
+head_node_ips=$(srun --nodes=1 --ntasks=1 -w "$head_node" hostname --all-ip-addresses)
+head_node_ips_array=($head_node_ips)
+head_node_ip=${head_node_ips_array[0]}
+
+srun -l --ntasks-per-node=1  Training/jobs/torchrun.sh 10.10.0.101     ##### sometime manula ip is needed, ip discover can be finicky
